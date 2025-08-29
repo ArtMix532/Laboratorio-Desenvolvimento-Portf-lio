d5c694d1777f551e1eff4daa98e4c47be08cb11e
Davi Benjamim Guimarães <154901283+ArtMix532@users.noreply.github.com>
2025-08-29 08:43:57 -0300

    Update constants.jsx

--- Codigo/src/data/constants.jsx
+++ Codigo/src/data/constants.jsx
@@ -34,8 +34,8 @@ export const Time_Line = [
         />
       </svg>
     ),
-    title: "Escola",
-    text: "Santa Mamnaf",
+    title: "School",
+    text: "Colégio Santa Maria Minas.",
   },
   {
     icon: (
@@ -54,8 +54,8 @@ export const Time_Line = [
         />
       </svg>
     ),
-    title: "Faculdade",
-    text: "Santa Mamnaf",
+    title: "Faculty of Software Engineering",
+    text: "I entered the Pontifical Catholic University of Minas Gerais in February 2024.",
   },
   {
     icon: (
@@ -74,7 +74,27 @@ export const Time_Line = [
         />
       </svg>
     ),
-    title: "Estágio ",
-    text: " ssudusdjusdju",
+    title: "IT Support Internship",
+    text: "I started as an IT support intern in June 2024 and stayed until August 2025.",
+  },
+  {
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
+        />
+      </svg>
+    ),
+    title: "STR-Sistemas e VT-Real",
+    text: "I started as a QA intern in August 2025 and I'm currently still in that role.",
   },
 ];
