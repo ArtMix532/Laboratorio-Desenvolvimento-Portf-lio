3f6167bcb35189fa48a90cff95dace9725c721b5
Davi Benjamim Guimarães <154901283+ArtMix532@users.noreply.github.com>
2025-08-29 13:31:06 -0300

    Update ProfessionalResume.jsx

--- Codigo/src/components/ProfessionalResume.jsx
+++ Codigo/src/components/ProfessionalResume.jsx
@@ -4,13 +4,15 @@ import Wave from "../assets/wave14.svg";
 function Resume() {
   return (
     <>
-      <div className=" bg-slate-300">
+      <div id="resume" className="  bg-slate-300">
         <img src={Wave} alt="" />
       </div>
       <div className=" bg-slate-700">
         <div className="px-10 lg:px-32">
           <div className="flex mb-4">
-            <h1 className="text-gray-400 font-bold font-ubuntu text-3xl ">Resume</h1>
+            <h1 className="text-gray-400 font-bold font-ubuntu text-3xl ">
+              Resume
+            </h1>
           </div>
         </div>
         <div className=""></div>
